5a786adddc55c5baee07e038d087dfa59dfd50b7
24f3000167 <24f3000167@ds.study.iitm.ac.in>
2026-07-21 04:21:15 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # The only two things you must fill in:
 EMAIL = "24f3000167@ds.study.iitm.ac.in"
-AIPIPE_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6IjI0ZjMwMDAxNjdAZHMuc3R1ZHkuaWl0bS5hYy5pbiIsImlhdCI6MTc4NDU4Njk2NiwiaXNzIjoiaHR0cHM6Ly9haXBpcGUub3JnIiwiYXVkIjoiYWlwaXBlLWFwaSIsImV4cCI6MTc4NTE5MTc2Nn0.t6UMOztppv5zxYVppSrV7V11bv0pt5iEmlCWsQzn-ME"
+AIPIPE_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6IjI0ZjMwMDAxNjdAZHMuc3R1ZHkuaWl0bS5hYy5pbiIsImlhdCI6MTc4NDU4Nzg0NSwiaXNzIjoiaHR0cHM6Ly9haXBpcGUub3JnIiwiYXVkIjoiYWlwaXBlLWFwaSIsImV4cCI6MTc4NTE5MjY0NX0.7LukUv4bhanUu-EY8qNmjiInFqpKoZu_Dww8nebNPDs"
 
 # Fixed — do not change
 AIPIPE_BASE = "https://aipipe.org/openai/v1"
